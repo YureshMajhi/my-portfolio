10e4001f3f105c5601a666a25987ec79b9d46b33
YureshMajhi <yureshmee@gmail.com>
2023-05-24 16:49:58 +0545

    Added navbar skeleton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/Yuresh.png";
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Yuresh Logo" className="w-[150px]" />
+      </div>
+
+      {/* menu bar */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* hamburger */}
+      <div className="md:hidden">
+        <FaBars />
+      </div>
+
+      {/* mobile menu */}
+      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* social icons */}
+      <div className="hidden"></div>
+    </div>
   );
 };
 
