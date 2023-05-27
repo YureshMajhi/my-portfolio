685f18df7b51332f8a0fbf7cb042b87232b5c54c
YureshMajhi <yureshmee@gmail.com>
2023-05-27 21:17:27 +0545

    About section Completed

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen flex flex-col justify-center items-center text-gray-300  bg-[#0a192f]"
+    >
+      <div className="w-full max-w-[1000px] px-4 grid grid-cols-2 gap-8">
+        <div className="sm:text-right pl-4 pb-8">
+          <p className="font-bold text-4xl border-b-4 border-pink-600 inline">
+            About
+          </p>
+        </div>
+        <div></div>
+      </div>
+      <div className="w-full max-w-[1000px] px-4 grid sm:grid-cols-2 gap-8">
+        <div className="sm:text-right">
+          <p className="font-bold text-4xl">
+            Hi, I'm Yuresh. Nice to meet you. Take a look around, if you'd like.
+          </p>
+        </div>
+        <div>
+          <p>
+            I love building great websites that make a positive impact. My
+            specialty is front-end development, serving everyone from
+            individuals to big companies. Imagine having a front-end expert
+            right at your fingertips. What would you do with that kind of skill?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
